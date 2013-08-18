bf66b7fb4cc61f955c1d4fd7f80812492be6da95
Satoshi Baba <babiy3104@gmail.com>
2013-08-18 15:03:19 +0900

    * 評者ランキングメソッドの実装

--- recommendation.rb
+++ recommendation.rb
@@ -1,5 +1,4 @@
 class Recommendation
-
     # ユークリッド空間
     def sim_distance prefs,person1,person2
         si={}
@@ -29,17 +28,30 @@ class Recommendation
         n = si.size
         return 0 if n == 0
 
+        # スコアの合計
         sum1 = si.keys.inject(0) { |s,i| s += prefs[person1][i] }
         sum2 = si.keys.inject(0) { |s,i| s += prefs[person2][i] }
 
+        # 各作品スコアの平方の合計
         sum1Sq = si.keys.inject(0) { |s,i| s += prefs[person1][i]**2 }
         sum2Sq = si.keys.inject(0) { |s,i| s += prefs[person2][i]**2 }
 
+        # 作品毎の積
         pSum = si.keys.inject(0) { |s,i| s += prefs[person1][i] * prefs[person2][i] }
         num = pSum - (sum1*sum2/n.to_f)
         den = Math::sqrt((sum1Sq - (sum1**2)/n.to_f)*(sum2Sq - (sum2**2)/n.to_f))
         return 0 if den == 0
 
         return num/den
+    end
+
+    # nameとの類似性スコアと相手の名前を取得する
+    #   similaliry 相関値関数の選択 
+    #   n          出力数
+    def top_matches prefs,name,n=3,similarity=:sim_pearson
+        scores = (prefs.keys - [name]).map { |other|
+            [self.send(similarity, prefs, name, other), other] 
+        }
+        return scores.sort.reverse[0...n]
     end
 end
--- test_recommendation.rb
+++ test_recommendation.rb
@@ -49,4 +49,54 @@ describe "Test recommendation" do
         }
         expect(rec.sim_pearson prefs,"斎藤 ","山田").to eq(0)
     end
+    it "test top_maches normal by sim_pearson" do
+        rec = Recommendation.new
+        prefs = { 
+            "斎藤"=> { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }, 
+            "山田"=> { "謎解き"=> 3, "相棒"=> 4, "真夏の方程式"=> 2, "坂の上から"=> 5 },
+            "岡田"=> { "謎解き"=> 5, "相棒"=> 2, "真夏の方程式"=> 4 },
+            "石川"=> { "謎解き"=> 2, "相棒"=> 4, "真夏の方程式"=> 3 },
+            "富田"=> { "男はつらいよ"=> 5, "謎解き"=> 5, "相棒"=>1,"真夏の方程式"=> 1 },
+            "大島"=> { "男はつらいよ"=> 2, "謎解き"=> 3, "相棒"=>5,"真夏の方程式"=> 2 },
+            "渡辺"=> { "謎解き"=> 4, "相棒"=>3,"真夏の方程式"=> 4,"坂の上から"=>4 }
+        }
+        expect(rec.top_matches(prefs, "斎藤",1)).to eq([[0.9449111825230686,"大島"]])
+    end
+    it "test sim_pearson same score" do
+        rec = Recommendation.new
+        prefs = { 
+            "斎藤" => { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }, 
+            "山田" => { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }
+        }
+        expect(rec.top_matches(prefs, "斎藤",1)).to eq([[1,"山田"]])
+    end
+    it "test sim_pearson mismatch score" do
+        rec = Recommendation.new
+        prefs = { 
+            "斎藤" => { "謎解き"=> 4 }, 
+            "山田" => { "相棒"=> 5 }
+        }
+        expect(rec.top_matches(prefs, "斎藤",1)).to eq([[0,"山田"]])
+    end
+    it "test sim_pearson another score" do
+        rec = Recommendation.new
+        prefs = { 
+            "斎藤" => { "相棒"=> 1 }, 
+            "山田" => { "相棒"=> 5 }
+        }
+        expect(rec.top_matches(prefs, "斎藤",1)).to eq([[0,"山田"]])
+    end
+    it "test top_maches normal by sim_distance" do
+        rec = Recommendation.new
+        prefs = { 
+            "斎藤"=> { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }, 
+            "山田"=> { "謎解き"=> 3, "相棒"=> 4, "真夏の方程式"=> 2, "坂の上から"=> 5 },
+            "岡田"=> { "謎解き"=> 5, "相棒"=> 2, "真夏の方程式"=> 4 },
+            "石川"=> { "謎解き"=> 2, "相棒"=> 4, "真夏の方程式"=> 3 },
+            "富田"=> { "男はつらいよ"=> 5, "謎解き"=> 5, "相棒"=>1,"真夏の方程式"=> 1 },
+            "大島"=> { "男はつらいよ"=> 2, "謎解き"=> 3, "相棒"=>5,"真夏の方程式"=> 2 },
+            "渡辺"=> { "謎解き"=> 4, "相棒"=>3,"真夏の方程式"=> 4,"坂の上から"=>4 }
+        }
+        expect(rec.top_matches(prefs, "斎藤",1,:sim_distance)).to eq([[0.2,"渡辺"]])
+    end
 end
