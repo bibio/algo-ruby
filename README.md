025bff1aba84db4e169ffcdd5a15535a870e7455
Satoshi Baba <babiy3104@gmail.com>
2013-08-18 13:44:56 +0900

    READMEの追加

---
+++ README.md
@@ -0,0 +1,12 @@
+* bellfordman.rb
+
+ベルマン・フォード法の実装
+参照：http://ja.wikipedia.org/wiki/%E3%83%99%E3%83%AB%E3%83%9E%E3%83%B3-%E3%83%95%E3%82%A9%E3%83%BC%E3%83%89%E6%B3%95
+
+* recommendation.rb
+
+推薦系
+ユークリッド距離
+ピアソン相関
+
+参照：集合知プログラミング Toby Segaran, オライリージャパン
--- test_recommendation.rb
+++ test_recommendation.rb
@@ -4,49 +4,49 @@ describe "Test recommendation" do
     it "test sim_distance normal" do
         rec = Recommendation.new
         prefs = { 
-            "馬場"=> { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }, 
+            "斎藤 "=> { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }, 
             "山田"=> { "謎解き"=> 3, "相棒"=> 4, "真夏の方程式"=> 2, "坂の上から"=> 5 }
         }
-        expect(rec.sim_distance(prefs,"馬場","山田").round(7)).to eq(0.1428571)
+        expect(rec.sim_distance(prefs,"斎藤 ","山田").round(7)).to eq(0.1428571)
     end
     it "test sim_distance empty" do
         rec = Recommendation.new
         prefs = { 
-            "馬場"=> { "謎解き"=> 4 }, 
+            "斎藤 "=> { "謎解き"=> 4 }, 
             "山田"=> { "相棒"=> 4, }
         }
-        expect(rec.sim_distance prefs,"馬場","山田").to eq(0)
+        expect(rec.sim_distance prefs,"斎藤 ","山田").to eq(0)
     end
     it "test sim_distance same score" do
         rec = Recommendation.new
         prefs = { 
-            "馬場"=> { "相棒"=> 4 }, 
+            "斎藤 "=> { "相棒"=> 4 }, 
             "山田"=> { "相棒"=> 4 }
         }
-        expect(rec.sim_distance prefs,"馬場","山田").to eq(1)
+        expect(rec.sim_distance prefs,"斎藤 ","山田").to eq(1)
     end
     it "test sim_pearson normal" do
         rec = Recommendation.new
         prefs = { 
-            "馬場"=> { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }, 
+            "斎藤 "=> { "謎解き"=> 4, "相棒"=> 5, "真夏の方程式"=> 4 }, 
             "山田"=> { "謎解き"=> 3, "相棒"=> 4, "真夏の方程式"=> 2, "坂の上から"=> 5 }
         }
-        expect(rec.sim_pearson(prefs,"馬場","山田").round(7)).to eq(0.8660254)
+        expect(rec.sim_pearson(prefs,"斎藤 ","山田").round(7)).to eq(0.8660254)
     end
     it "test sim_pearson empty" do
         rec = Recommendation.new
         prefs = { 
-            "馬場"=> { "謎解き"=> 4 }, 
+            "斎藤 "=> { "謎解き"=> 4 }, 
             "山田"=> { "相棒"=> 4, }
         }
-        expect(rec.sim_pearson prefs,"馬場","山田").to eq(0)
+        expect(rec.sim_pearson prefs,"斎藤 ","山田").to eq(0)
     end
     it "test sim_pearson same score" do
         rec = Recommendation.new
         prefs = { 
-            "馬場"=> { "相棒"=> 4 }, 
+            "斎藤 "=> { "相棒"=> 4 }, 
             "山田"=> { "相棒"=> 4 }
         }
-        expect(rec.sim_pearson prefs,"馬場","山田").to eq(0)
+        expect(rec.sim_pearson prefs,"斎藤 ","山田").to eq(0)
     end
 end
